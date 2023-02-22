f262f3aa5a60844304d39d055b135be9b8879e35
artainmo <53705599+artainmo@users.noreply.github.com>
2023-02-22 13:52:50 +0100

    Delete find_slot directory

--- find_slot/src/info_access_token.sh
+++
@@ -1,9 +0,0 @@
-ACCESS_TOKEN=$1
-
-if [ $# -ne 1 ]; then
-    echo "MISSING ACCESS_TOKEN ARGUMENT"
-    exit
-fi
-
-#See all projects, slugs and ids
-curl -H "Authorization: Bearer $ACCESS_TOKEN" "https://api.intra.42.fr/oauth/token/info"
