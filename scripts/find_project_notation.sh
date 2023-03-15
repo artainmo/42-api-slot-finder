4ff74d7382b77062003064914b794b1060740cc3
artainmo <53705599+artainmo@users.noreply.github.com>
2023-03-15 21:59:33 +0100

    Update find_project_notation.sh

--- scripts/find_project_notation.sh
+++ scripts/find_project_notation.sh
@@ -3,4 +3,4 @@ printf "Login: "
 read LOGIN
 
 #See all projects, slugs and ids
-curl -H "Authorization: Bearer $API42_ACCESS_TOKEN" "https://api.intra.42.fr/v2/users/$LOGIN/projects_users"
+curl -H "Authorization: Bearer $API42_ACCESS_TOKEN" -g "https://api.intra.42.fr/v2/users/$LOGIN/projects_users?page[size]=100"
